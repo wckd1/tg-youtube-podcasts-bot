0d822011ece19d5d3785faedab98bf63afcee125
wckd1 <korobeinikovr@gmail.com>
2022-11-10 21:23:53 +0100

    Updated arguments parsing

--- feed/parser.go
+++ feed/parser.go
@@ -10,6 +10,21 @@ import (
 	"mvdan.cc/xurls/v2"
 )
 
+//
+// Supported links:
+//
+// Video:
+// /watch?v={id}
+//
+// Channel:
+// /c/{id}
+// /channel/{id}
+//
+// Playlist:
+// /watch?v={video_id}&list={id}
+// /playlist?list={id}
+//
+
 // Parse params to detect subscription details
 func (fs FeedService) parseSubscription(arguments string) (sub db.Subscription, err error) {
 	sub = db.Subscription{}
@@ -30,37 +45,33 @@ func (fs FeedService) parseSubscription(arguments string) (sub db.Subscription, 
 
 	// Check link type
 	path := strings.Split(purl.Path, "/")[1]
-	switch path {
-	// Check if passed video link
-	case "watch":
-		// Check if video is in playlist
-		// TODO: Add support for https://www.youtube.com/playlist?list= links
-		if _, ok := purl.Query()["list"]; ok {
-			err = fmt.Errorf("playlist subscription is not implemented yet")
-			// sub.Type = db.Playlist
-			// sub.YouTubeID = listID[0]
-		} else if videoID, ok := purl.Query()["v"]; ok {
-			sub.Type = db.Video
-			sub.YouTubeID = videoID[0]
-		} else {
-			err = fmt.Errorf("unrecognized link type")
-		}
-	// Check if passed channel link
-	case "c", "channel":
-		err = fmt.Errorf("channel subscription is not implemented yet")
-		// sub.Type = db.Channel
 
-		// // Parse title
-		// filter := strings.ReplaceAll(arguments, furl, "")
-		// sub.Filter = strings.TrimSpace(filter)
-
-		// channelID := strings.Split(purl.Path, "/")[2]
-		// sub.YouTubeID = channelID
-	// Other links are unsupported
-	default:
-		err = fmt.Errorf("unrecognized link type")
+	// Check for valid playlist link
+	listID, ok := purl.Query()["list"]
+	if ok && (path == "watch" || path == "playlist") {
+		sub.Type = db.Playlist
+		sub.YouTubeID = listID[0]
+		return
 	}
 
+	// Check for valid video link
+	videoID, ok := purl.Query()["v"]
+	if ok && path == "watch" {
+		sub.Type = db.Video
+		sub.YouTubeID = videoID[0]
+	}
+
+	// Check for valid channel link
+	if path == "c" || path == "channel" {
+		sub.Type = db.Channel
+		sub.YouTubeID = strings.Split(purl.Path, "/")[2]
+
+		// Parse optional filter
+		sub.Filter = strings.ReplaceAll(arguments, furl, "")
+	}
+
+	// No supported links found
+	err = fmt.Errorf("unrecognized link type")
 	return
 }
 
